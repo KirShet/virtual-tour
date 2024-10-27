a29c0e99b72ce756d56d4dd4e8199abb5fed18b1
KirShet <kirshet2000@gmail.com>
2024-10-28 01:10:19 +0600

    Creating a HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cage;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        $cage = Cage::width('animals')->get();
+
+        return view('home', compact('cages'));
+    }
 }
