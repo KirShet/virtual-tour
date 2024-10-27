3b7251c9ae0158e732d71a429b0142ec6b723809
KirShet <kirshet2000@gmail.com>
2024-10-28 02:38:56 +0600

    Fixing a bug in HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $cage = Cage::width('animals')->get();
+        $cage = Cage::with('animals')->get();
 
         return view('home', compact('cages'));
     }
