6e414b5478b9de8c40f871a6f24450ae5ee60bcc
KirShet <kirshet2000@gmail.com>
2024-10-28 02:57:15 +0600

    Edited view index.blade.php

--- resources/views/cages/index.blade.php
+++ resources/views/cages/index.blade.php
@@ -10,6 +10,7 @@
         <div>
             <h2>{{ $cage->name }}</h2>
             <p>Capacity: {{$cage->capacity}} </p>
+            <p>Count of animals: {{ $cage->animals_count }}</p>
             <a href="{{ route('cages.show', $cage) }}" class="btn btn-info">Watch</a>
             <a href="{{ route('cages.edit', $cage) }}" class="btn btn-warning"> Edit</a>
             <form action="{{ route('cages.destroy', $cage) }}" method="POST" style="display:inline;">
