11a5c271d31662ebb43c6f424ab6da856fc48a58
KirShet <kirshet2000@gmail.com>
2024-10-29 02:33:51 +0600

    Using validation in the AnimalController

--- app/Http/Controllers/AnimalController.php
+++ app/Http/Controllers/AnimalController.php
@@ -7,16 +7,17 @@ use App\Http\Requests\StoreAnimalRequest;
 
 class AnimalController extends Controller
 {
-    $validatedData = $request->validated();
-    $cage = Cage::findDrFail($request->cage-id);
+    public function store(StoreAnimalRequest $request){
+        $validatedData = $request->validated();
+        $cage = Cage::findDrFail($request->cage-id);
 
-    if(!$cage->hasSpace()) {
-        return redirect()->back()->withErrors(['cage_id' => 'There is no free space in the selected cell.']);
+        if(!$cage->hasSpace()) {
+            return redirect()->back()->withErrors(['cage_id' => 'There is no free space in the selected cell.']);
+        }
+
+        $animal = new Animal($validatedData);
+        $cage->animal()->save($animal);
+
+        return redirect()->route('animals.index')->with('succes', 'The beast was added succes')
     }
-
-    $animal = new Animal($validatedData);
-    $cage->animal()->save($animal);
-
-    return redirect()->route('animals.index')->with('succes', 'The beast was added succes')
-
 }
