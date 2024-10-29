af0dc21ddef2b52d38fdf03e9aa676ecf392531b
KirShet <kirshet2000@gmail.com>
2024-10-30 00:08:10 +0600

    Создание view animals.index

---
+++ resources/views/animal/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>List animals</h1>
+    <a href="{{route('animals.create')}}">Add new beast</a>
+    @foreach ($animals as animal)
+        <h2>{{ $animal->name }}</h2>
+        <p><strong>Species:</strong>{{ $animal->species }}</p>
+        <p><strong>Age:</strong>{{ $animal->age }}</p>
+        <p><strong>Description:</strong>{{ $animal->description }}</p>
+        @auth
+            <a href="{{ route('animals.show', $animal) }}">Watch</a>
+            <a href="{{ route('animals.edit', $animal) }}">Edit</a>
+            <form action="{{ route('animals.destroy', $animal) }}" method="post">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger">Delete</button>
+            </form>
+        @endauth
+    @endforeach
+@endsection
--- resources/views/cages/index.blade.php
+++ resources/views/cages/index.blade.php
@@ -12,12 +12,14 @@
             <p>Capacity: {{$cage->capacity}} </p>
             <p>Count of animals: {{ $cage->animals_count }}</p>
             <a href="{{ route('cages.show', $cage) }}" class="btn btn-info">Watch</a>
-            <a href="{{ route('cages.edit', $cage) }}" class="btn btn-warning"> Edit</a>
-            <form action="{{ route('cages.destroy', $cage) }}" method="POST" style="display:inline;">
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="btn btn-danger">Delete</button>
-            </form>
+            @auth
+                <a href="{{ route('cages.edit', $cage) }}" class="btn btn-warning"> Edit</a>
+                <form action="{{ route('cages.destroy', $cage) }}" method="POST" style="display:inline;">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            @endauth
         </div>
     @endforeach
 @endsection
