97a73886dab5734fb67db1c34479ebeefba8de05
KirShet <kirshet2000@gmail.com>
2024-10-29 14:15:47 +0600

    AnimalController Update

--- app/Http/Controllers/AnimalController.php
+++ app/Http/Controllers/AnimalController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AnimalRequest;
 use Illuminate\Http\Request;
 use App\Http\Requests\StoreAnimalRequest;
 use App\Models\Animal;
@@ -37,5 +38,27 @@ class AnimalController extends Controller
     public function edit(Animal $animal)
     {
         return view('animals.edit', compact('animal'));
+    }
+
+    public function update(AnimalRequest $request, Animal $animal)
+    {
+        $validatedData = $request->validated();
+
+        if ($animal->cage_id !== $validatedData['cage_id']){
+            $newCage = Cage::findOrFail($validatedData['cage_id']);
+
+            if (!$newCage->hasSpace()){
+                return redirect()->back()->withErrors([ 'cage' => 'There is no free space in the selected cell.' ])
+            }
+        }
+        $animal->update($validatedData);
+        return redirect()->route('animal.show', $animal)->with('success', 'The beast was added succes')
+    }
+
+    public function destroy(Animal $animal)
+    {
+        $animal->delete();
+
+        return redirect()->route('animals.index')->with('success', 'The animal has been removed')
     }
 }
