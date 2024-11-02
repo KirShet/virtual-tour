ca287281e2ff9f21ec28d308e9286fff2ad60c8d
KirShet <kirshet2000@gmail.com>
2024-11-03 02:54:57 +0600

    Styled view animal.show and added deletion functions

--- resources/views/cages/show.blade.php
+++ resources/views/cages/show.blade.php
@@ -1,7 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{ $cage->name }}</h1>
-    <p>Capacity: {{ $cage->capacity }}</p>
-    <a href="{{ route('cages.index') }}">Go back</a>
+    <div class="d-flex justify-content-center align-items-center">
+        <div class="w-100 max-w-4xl">
+        <h1 class="h5 font-weight-semibold">{{ $cage->name }}</h1>
+        <p><strong>Capacity:</strong> {{ $cage->capacity }}</p>
+            <div class="mt-2">
+                <a href="{{ route('cages.index') }}" class="text-orange-600 hover:underline" >Go back</a>
+                <form action="{{ route('cages.destroy', $cage) }}" method="POST" class="d-inline ml-3">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger btn-sm">Удалить</button>
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
