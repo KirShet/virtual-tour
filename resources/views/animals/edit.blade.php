c298e3174ef1dedb610197a0c8e25d7020a0c852
KirShet <kirshet2000@gmail.com>
2024-11-02 21:44:28 +0600

    Styled animal edit

--- resources/views/animals/edit.blade.php
+++ resources/views/animals/edit.blade.php
@@ -1,25 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Edit animal</h1>
-    <form action="{{ route('animals.update', $animal) }}" method="POST">
+<div class="d-flex justify-content-center align-items-center vh-100">
+        <div class="w-100 max-w-4xl">
+            <h1 class="h3 font-weight-bold mb-4 text-center">Edit animal</h1>
+            @auth
+            <form action="{{ route('animals.update', $animal) }}" method="POST"  class="d-inline ml-3">
         @csrf
         @method('PUT')
+        <div class="p-5 border border-secondary rounded shadow-sm mb-4">
+            <label for="name"><strong>Name of the animal:</strong></label>
+            <input type="text" name="name" value="{{ $animal->name }}" required>
 
-        <label for="name">Name of the animal:</label>
-        <input type="text" name="name" value="{{ $animal->name }}" required>
+            <label for="species"><strong>Species: </strong></label>
+            <input type="text" name="species" value="{{ $animal->species }}" required>
 
-        <label for="species">Species: </label>
-        <input type="text" name="species" value="{{ $animal->species }}" required>
+            <label for="Age"><strong>Age:</strong></label>
+            <input type="number" name="age" value="{{ $animal->age }}" required>
 
-        <label for="Age">Age:</label>
-        <input type="number" name="age" value="{{ $animal->age }}" required>
+            <label for="description"><strong>Description: </strong></label>
+            <input type="text" name="description" value="{{ $animal->description }}" required>
 
-        <label for="description">description: </label>
-        <input type="text" name="description" value="{{ $animal->description }}" required>
-
-        <label for="cage_id">Cage ID:</label>
-
+            <label for="cage_id">Cage ID:</label>
+        
         <select name="cage_id" required>
             <option value="{{ $animal->cage_id }}">{{current(array_filter($cages->toArray(), fn($cage) => $cage['id'] == $animal->cage_id))['name']}}</option>
             @foreach ($cages as $cage)
@@ -28,6 +31,8 @@
                 @endif
             @endforeach
         </select>
-        <button type="submit">Update</button>
+        <button type="submit"  class="btn btn-danger btn-sm">Update</button>
     </form>
+    @endauth
+    </div>
 @endsection
