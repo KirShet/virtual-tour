49c677d7790ee444be950092ab781609d0227514
KirShet <kirshet2000@gmail.com>
2024-11-03 01:57:48 +0600

    Styled view cages.create

--- resources/views/cages/create.blade.php
+++ resources/views/cages/create.blade.php
@@ -1,15 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Add cage</h1>
-    <form action="{{ route('cages.store')}}" method="post">
-        @csrf
-        <label for="name">Name of the cell:</label>
-        <input type="text" name="name" required>
+    <div class="d-flex justify-content-center align-items-center">
+        <div class="w-100 max-w-4xl">
+            <h1 class="h3 font-weight-bold mb-4 text-center">Add cage</h1>
+            <div class="mt-4">
+                @auth
+                <form action="{{ route('animals.store') }}" method="POST" class="p-5 border border-secondary rounded shadow-sm mb-4">
+                    @csrf
+                    <div class="form-group">
+                        <label for="name"><strong>Name of the cell:</strong></label>
+                        <input type="text" name="name" required class="form-control">
+                    </div>
 
-        <label for="capacity">Capacity: </label>
-        <input type="number" name="capacity" required>
-
-        <button type="submit">Add Cage</button>
-    </form>
+                    <div class="form-group">
+                        <label for="species"><strong>Capacity:</strong></label>
+                        <input type="text" name="capacity"  required class="form-control">
+                    </div>
+                    <div class="mt-4 text-center">
+                        <button type="submit" class="btn btn-danger btn-sm">Add Cage</button>
+                    </div>
+                </form>
+                @endauth
+            </div>
+        </div>
+    </div>
 @endsection
