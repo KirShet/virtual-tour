f8f23fa7ee32b278b42fbf58329b02f5bfd79f59
KirShet <kirshet2000@gmail.com>
2024-11-03 03:01:56 +0600

    Styled view animal.show

--- resources/views/cages/show.blade.php
+++ resources/views/cages/show.blade.php
@@ -3,15 +3,19 @@
 @section('content')
     <div class="d-flex justify-content-center align-items-center">
         <div class="w-100 max-w-4xl">
-        <h1 class="h5 font-weight-semibold">{{ $cage->name }}</h1>
-        <p><strong>Capacity:</strong> {{ $cage->capacity }}</p>
-            <div class="mt-2">
-                <a href="{{ route('cages.index') }}" class="text-orange-600 hover:underline" >Go back</a>
-                <form action="{{ route('cages.destroy', $cage) }}" method="POST" class="d-inline ml-3">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-danger btn-sm">Удалить</button>
-                </form>
+            <h1 class="h5 font-weight-semibold">{{ $cage->name }}</h1>
+            <div class="mt-4">
+                <div class="p-5 border border-secondary rounded shadow-sm mb-4">
+                <p><strong>Capacity:</strong> {{ $cage->capacity }}</p>
+                    <div class="mt-2">
+                        <a href="{{ route('cages.index') }}" class="text-orange-600 hover:underline" >Go back</a>
+                        <form action="{{ route('cages.destroy', $cage) }}" method="POST" class="d-inline ml-3">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger btn-sm">Удалить</button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
