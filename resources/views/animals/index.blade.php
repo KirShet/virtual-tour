0e7b5de90c98e15db22bc8f41a4d5fa894700d19
KirShet <kirshet2000@gmail.com>
2024-11-02 18:54:36 +0600

    Added animal list view with Bootstrap styling.

--- resources/views/animals/index.blade.php
+++ resources/views/animals/index.blade.php
@@ -1,6 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="d-flex justify-content-center align-items-center vh-100">
+    <div class="w-100" style="max-width: 800px;">
+        <h1 class="h3 font-weight-bold mb-4 text-center">List Animal</h1>
+        <div class="mb-4 text-center">
+            <a href="{{ route('animals.create')}}" class="btn btn-success"></a>
+        </div>
+        <div class="mt-4">
+            @foreach ($animals as $animal )
+            <div class="p-3 border border-secondary rounded shadow-sm mb-4"></div>
+            <h2 class="h5 font-weight-semibold">{{ $animal->name }}</h2>
+            <p><strong>Species: </strong> {{$animal->species }}</p>
+            <p><strong>Age: </strong> {{$animal->age }}</p>
+            <p><strong>Description: </strong> {{$animal->description }}</p>
+
+            @endforeach
+        </div>
+    </div>
+</div>
     <h1>List animals</h1>
     <a href="{{route('animals.create')}}">Add new beast</a>
     @foreach ($animals as $animal)
@@ -8,8 +26,8 @@
         <p><strong>Species:</strong>{{ $animal->species }}</p>
         <p><strong>Age:</strong>{{ $animal->age }}</p>
         <p><strong>Description:</strong>{{ $animal->description }}</p>
+        <a href="{{ route('animals.show', $animal) }}">Watch</a>
         @auth
-            <a href="{{ route('animals.show', $animal) }}">Watch</a>
             <a href="{{ route('animals.edit', $animal) }}">Edit</a>
             <form action="{{ route('animals.destroy', $animal) }}" method="post">
                 @csrf
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,10 @@
     <body class="font-sans antialiased">
         <nav class="navbar navbar-expand-lg navbar-light bg-light" ">
             <a class="navbar-brand" href="{{ route('home') }}"> Virtual Tour </a>
-            <div class="navbar-collapse">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('home') }}">Main page</a>
