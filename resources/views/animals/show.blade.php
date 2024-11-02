6bc2447a2dcd15cf76e25542b697a555d28df960
KirShet <kirshet2000@gmail.com>
2024-11-02 21:15:47 +0600

    Styled animal show

--- resources/views/animals/show.blade.php
+++ resources/views/animals/show.blade.php
@@ -1,15 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-        <h1>{{ $animal->name }}</h1>
-        <p>Species: {{ $animal->species }}</p>
-        <p>Age: {{ $animal->age }}</p>
-        <p>Descriptions: {{ $animal->description }}</p>
-        <a href="{{ route('animals.index') }}">Go back</a>
-        <form action="{{ route('animals.destroy', $animal) }}" method="POST">
-            @csrf
-            @method('DELETE')
-            <button type="submit">Удалить</button>
-        </form>
-        <a href="{{ route('animals.edit', $animal) }}"></a>
+    <div class="d-flex justify-content-center align-items-center vh-100">
+        <div class="w-100 max-w-4xl">
+            <h1 class="h5 font-weight-semibold">{{ $animal->name }}</h1>
+            <div class="mt-4">
+                <div class="p-5 border border-secondary rounded shadow-sm mb-4">
+                    <p><strong>Species:</strong> {{ $animal->species }}</p>
+                    <p><strong>Age:</strong> {{ $animal->age }}</p>
+                    <p><strong>Description:</strong> {{ $animal->description }}</p>
+                    <div class="mt-2">
+                        <a href="{{ route('animals.index') }}" class="text-orange-600 hover:underline" >Go back</a>
+                    </div>
+                    <form action="{{ route('animals.destroy', $animal) }}" method="POST" class="d-inline ml-3">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger btn-sm">Удалить</button>
+                    </form>
+                    <a href="{{ route('animals.edit', $animal) }}" class="ml-3 text-warning" >Edit</a>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse">
+            <div class="navbar-collapse">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('home') }}">Main page</a>
