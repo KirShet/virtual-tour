6aec25b06bf1bb49de56a330d665e63759901c28
KirShet <kirshet2000@gmail.com>
2024-11-03 02:20:52 +0600

    Update file structure and remove edit form elements

--- resources/views/cages/edit.blade.php
+++ resources/views/cages/edit.blade.php
@@ -25,20 +25,4 @@
         </div>
     </div>
 </div>
-
-
-
-    <h1>Edit cage</h1>
-    <form action="{{ route('cages.update', $cage) }}" method="POST">
-        @csrf
-        @method('PUT')
-
-        <label for="name">Name</label>
-        <input type="text" name="name" value="{{ $cage->name }}" required>
-
-        <label for="capacity">capacity</label>
-        <input type="number" name="capacity" value="{{ $cage->capacity }}" required>
-
-        <button type="submit">Update</button>
-    </form>
 @endsection
